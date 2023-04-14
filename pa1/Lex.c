e9ba5936b124661ce635f5830dae0f0f27776716
Chris <ckmoon@ucsc.edu>
2023-04-14 03:32:33 -0700

    temp

---
+++ pa1/Lex.c
@@ -0,0 +1,131 @@
+//Christopher Moon
+//ckmoon
+//pa1
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
+
+#include"List.h"
+#define MAX_LEN 300
+
+int main(int argc, char** argv){
+
+  //file pointers
+  FILE *infile; 
+  FILE *outfile; 
+
+  //make sure 2 args
+  if(argc != 3){
+  
+    printf("2 arguments not given\n");
+    
+    exit(1);
+    
+  }
+
+  //open files
+  infile = fopen(argv[1], "r");
+  
+  outfile = fopen(argv[2], "w");
+  
+  //check if files are valid
+  if(infile==NULL || outfile==NULL){
+  
+    printf("bad files\n");
+    
+    exit(1);
+    
+  }
+
+  //count lines of infile
+  
+  char buf[300];
+  
+  int n = 0;
+  
+  while(fgets(buf, 300, infile) != NULL){
+      	
+    n++;
+  
+  }
+  
+  printf("number of lines is %d\n", n);
+  
+  //create string array of length n
+  char *arr = (char*) malloc( sizeof(char) * n);
+  
+  
+  //infile reset
+  fclose(infile);
+  infile = fopen(argv[1], "r");
+
+  //read in lines of infile into string array
+  int lines = 0;
+  
+  while(fgets(buf, 300, infile) != NULL){
+    
+    printf("buf value is %s\n", buf);
+    
+    strcpy(&arr[lines], buf);
+    
+    //buf = buf[300];
+    lines++;
+    
+  }
+  
+  printf("string array is %s\n", arr);
+  
+  //create list
+  List l = newList();
+
+  //sort?
+  for (int i = 0; i < n; i++){
+  
+    moveBack(l);
+    
+    while(index(l) >-1 && strcmp(&arr[i], &arr[get(l)]) < 0){
+      
+      movePrev(l);
+      
+    }
+    
+    if(index(l) > -1){
+    
+      insertAfter(l, i);
+      
+    }else{
+    
+      prepend(l, i);
+      
+    }
+    
+  }
+
+  //output
+  
+  moveFront(l);
+  
+  while (index(l) > -1){
+  
+    fprintf(outfile, "%s", &arr[get(l)]);
+    
+    moveNext(l);
+    
+  }
+  
+  //free arr
+  free(arr);
+
+  // free list
+  freeList(&l);
+
+  // close files
+  fclose(infile);
+  
+  fclose(outfile);
+
+  return 0;
+
+}
